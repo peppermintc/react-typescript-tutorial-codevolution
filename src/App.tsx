2eeaebcc47d865b7389e1dbfb3e21488c4416a99
peppermintc <haminjp@naver.com>
2021-12-12 15:51:05 +0900

    wrapping html element

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import { Private } from "./components/auth/Private";
 import { List } from "./components/generics/List";
 import { RandomNumber } from "./components/restriction/RandomNumber";
 import { Toast } from "./components/templateliterals/Toast";
+import { CustomButton } from "./components/html/Button";
 
 function App() {
   const personName = {
@@ -88,7 +89,11 @@ function App() {
       /> */}
 
       {/* <RandomNumber value={10} isPositive /> */}
-      <Toast position="center" />
+      {/* <Toast position="center" /> */}
+
+      <CustomButton variant="primary" onClick={() => console.log("hi")}>
+        content
+      </CustomButton>
     </div>
   );
 }
